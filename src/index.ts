1dd6246fab9e054379157e9551a335c474dd6a05
Isaac-Lee <isaac.lee.3412@gmail.com>
2022-09-15 17:06:21 +0900

    add main code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+import { BlockChain } from './chain/BlockChain';
+
+const blockchain = new BlockChain();
+blockchain.addBlock('First block');
+blockchain.addBlock('Second block');
+blockchain.addBlock('Third block');
+blockchain.addBlock('Fourth block');
+
+console.log(blockchain.getBlocks());
